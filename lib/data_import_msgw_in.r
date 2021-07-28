1421f015b6f4198cc23fb2e79a320a194d567965
Wes Cravens <wcravens@cortex-it.com>
2021-07-27 23:48:35 -0500

    Various stuff getting ready for analysis

--- lib/data_import_msgw_in.r
+++ lib/data_import_msgw_in.r
@@ -19,17 +19,24 @@ supplement <- function( msgw_in ) {
   msgw_in_unique_length <- length( unique( msgw_in$ts_ns ) )
   print( paste( "msgw_in$ts_nw is Unique:", msgw_in_length == msgw_in_unique_length ) )
   print( paste( "Percent of msgw_in Unique ts_ns: ", msgw_in_unique_length / msgw_in_length ) )
-  
+   
   ## msgw_in_duplicated_ts_ns <- msgw_in[duplicated(msgw_in$ts_ns),]
   ## Visual inspection of the observations with duplicate time stamps did not reviel any obvious patters.  We will leave the duplicate timestamps as they are.
   
   # Normalize Timestamps to start at T0 and coerce into double (instead of bit64)
   msgw_in$t0 <- as.double( msgw_in$ts_ns - data.table::first(msgw_in$ts_ns) )
   
-  # Calculate interevent delta_t and coerce into double (instaead of bit64)
+  # Calculate intermittent delta_t and coerce into double (instaead of bit64)
   msgw_in$delta_t <- as.double( msgw_in$t0 - data.table::shift( msgw_in$t0, n=1, fill=0, type="lag") )
-  return (msgw_in)
+  
+  latency_in_ms <- as.numeric( msgw_in$`PNQM Latency (ms)` )
+  latency_null_to_zero <- latency_in_ms[ is.na(latency_in_ms) ] <- 0
+  latency_in_ns <- latency_in_ms * 1e6
+  msgw_in$t_exit <- msgw_in$t0 + latency_in_ns 
+  
+  return( msgw_in )
 }
+
 
 validate <- function( msgw_in ) {
   print( paste( "ts_ns is Monotonic: ", all(msgw_in$ts_ns == cummax(msgw_in$ts_ns) ) ) )
